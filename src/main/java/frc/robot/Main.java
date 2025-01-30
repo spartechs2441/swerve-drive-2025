b2acc24ff69b592869b23b0d0c2e8f33dd4a7acf
spartechs <2441spartechs@maryknollschool.org>
2025-01-29 15:00:52 -1000

    SIIIIIKKEEEE
    This reverts commit a3cf8b84ea6bcc7ae508a13fbc3dfbf989b27dad.

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -1,24 +1,26 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-package frc.robot
 
-import edu.wpi.first.wpilibj.RobotBase
+package frc.robot;
+
+import edu.wpi.first.wpilibj.RobotBase;
 
 /**
  * Do NOT add any static variables to this class, or any initialization at all. Unless you know what
  * you are doing, do not modify this file except to change the parameter class to the startRobot
  * call.
  */
-object Main {
+public final class Main {
+    private Main() {
+    }
+
     /**
      * Main initialization function. Do not perform any initialization here.
      *
-     *
-     * If you change your main robot class, change the parameter type.
+     * <p>If you change your main robot class, change the parameter type.
      */
-    @kotlin.jvm.JvmStatic
-    fun main(args: Array<String>) {
-        RobotBase.startRobot { Robot() }
+    public static void main(String... args) {
+        RobotBase.startRobot(Robot::new);
     }
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,12 +1,13 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-package frc.robot
 
-import edu.wpi.first.wpilibj.TimedRobot
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
-import edu.wpi.first.wpilibj2.command.Command
-import edu.wpi.first.wpilibj2.command.CommandScheduler
+package frc.robot;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -14,52 +15,66 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler
  * the package after creating this project, you must also update the build.gradle file in the
  * project.
  */
-class Robot : TimedRobot() {
-    private var m_autonomousCommand: Command? = null
+public class Robot extends TimedRobot {
+    private Command m_autonomousCommand;
 
-    private lateinit var m_robotContainer: RobotContainer
+    private RobotContainer m_robotContainer;
+
+    /**
+     * TODO
+     * Call this method whenever something that isn't supposed to happen, happens
+     * REPLACE THIS EXCEPTION WITH A PRINT STATEMENT DURING COMPETITION
+     */
+    public static void errorAssert(String message) {
+        System.out.println(message);
+        // throw new IllegalStateException(message);
+    }
 
     /**
      * This function is run when the robot is first started up and should be used for any
      * initialization code.
      */
-    override fun robotInit() {
+    @Override
+    public void robotInit() {
         // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
         // autonomous chooser on the dashboard.
-        m_robotContainer = RobotContainer()
+        m_robotContainer = new RobotContainer();
     }
 
     /**
      * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
      * that you want ran during disabled, autonomous, teleoperated and test.
      *
-     *
-     * This runs after the mode specific periodic functions, but before LiveWindow and
+     * <p>This runs after the mode specific periodic functions, but before LiveWindow and
      * SmartDashboard integrated updating.
      */
-    override fun robotPeriodic() {
-        SmartDashboard.putNumber("Gyro", m_robotContainer.gyro)
+    @Override
+    public void robotPeriodic() {
+        SmartDashboard.putNumber("Gyro", m_robotContainer.getGyro());
         // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
         // commands, running already-scheduled commands, removing finished or interrupted commands,
         // and running subsystem periodic() methods.  This must be called from the robot's periodic
         // block in order for anything in the Command-based framework to work.
-        CommandScheduler.getInstance().run()
+        CommandScheduler.getInstance().run();
     }
 
     /**
      * This function is called once each time the robot enters Disabled mode.
      */
-    override fun disabledInit() {
+    @Override
+    public void disabledInit() {
     }
 
-    override fun disabledPeriodic() {
+    @Override
+    public void disabledPeriodic() {
     }
 
     /**
-     * This autonomous runs the autonomous command selected by your [RobotContainer] class.
+     * This autonomous runs the autonomous command selected by your {@link RobotContainer} class.
      */
-    override fun autonomousInit() {
-        m_autonomousCommand = m_robotContainer.autonomousCommand
+    @Override
+    public void autonomousInit() {
+        m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
         /*
          * String autoSelected = SmartDashboard.getString("Auto Selector",
@@ -69,51 +84,46 @@ class Robot : TimedRobot() {
          */
 
         // schedule the autonomous command (example)
-        m_autonomousCommand?.schedule()
+        if (m_autonomousCommand != null) {
+            m_autonomousCommand.schedule();
+        }
     }
 
     /**
      * This function is called periodically during autonomous.
      */
-    override fun autonomousPeriodic() {
+    @Override
+    public void autonomousPeriodic() {
     }
 
-    override fun teleopInit() {
+    @Override
+    public void teleopInit() {
         // This makes sure that the autonomous stops running when
         // teleop starts running. If you want the autonomous to
         // continue until interrupted by another command, remove
         // this line or comment it out.
         if (m_autonomousCommand != null) {
-            m_autonomousCommand!!.cancel()
+            m_autonomousCommand.cancel();
         }
     }
 
     /**
      * This function is called periodically during operator control.
      */
-    override fun teleopPeriodic() {
+    @Override
+    public void teleopPeriodic() {
     }
 
-    override fun testInit() {
+    @Override
+    public void testInit() {
         // Cancels all running commands at the start of test mode.
-        CommandScheduler.getInstance().cancelAll()
+        CommandScheduler.getInstance().cancelAll();
     }
 
     /**
      * This function is called periodically during test mode.
      */
-    override fun testPeriodic() {
-    }
-
-    companion object {
-        /**
-         * TODO
-         * Call this method whenever something that isn't supposed to happen, happens
-         * REPLACE THIS EXCEPTION WITH A PRINT STATEMENT DURING COMPETITION
-         */
-        fun errorAssert(message: String?) {
-            println(message)
-            // throw new IllegalStateException(message);
-        }
+    @Override
+    public void testPeriodic() {
     }
 }
